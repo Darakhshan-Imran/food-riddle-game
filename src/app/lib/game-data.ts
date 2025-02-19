fae2ddf7da70fbc5c7f1c89a99674375c99ad537
Darakhshan-Imran <darakhshanimranid@gmail.com>
2025-02-20 01:26:06 +0500

    Changes updated

--- src/app/lib/game-data.ts
+++ src/app/lib/game-data.ts
@@ -7,7 +7,7 @@ export type RiddleItem = {
   
   export const riddleData: RiddleItem[] = [
     {
-      riddle: "I'm round, I'm red, and I'm full of seeds. What am I?",
+      riddle: "I am round, I'm red, and I'm full of seeds. What am I?",
       answer: "Tomato",
       benefit: "Rich in lycopene, which may reduce the risk of heart disease and certain cancers.",
       image: "/images/image1a.webp",
@@ -19,31 +19,31 @@ export type RiddleItem = {
       image: "/images/image2b.webp",
     },
     {
-      riddle: "I'm small, round, and green, Dried, I'm a raisin—I'm often seen! What am I?",
+      riddle: "I am small, round, and green, Dried, I'm a raisin—I'm often seen! What am I?",
       answer: "Grape",
       benefit: "High in resveratrol, an antioxidant that may protect against heart disease.",
       image: "/images/image3b.webp",
     },
     {
-      riddle: "I'm white, creamy, and made from milk. What am I?",
+      riddle: "I am white, creamy, and made from milk. What am I?",
       answer: "Yogurt",
       benefit: "Contains probiotics that support gut health and boost the immune system.",
       image: "/images/image4b.webp",
     },
     {
-      riddle: "I'm green, leafy, and full of iron. What am I?",
+      riddle: "I am green, leafy, and full of iron. What am I?",
       answer: "Spinach",
       benefit: "Packed with iron, which helps transport oxygen throughout the body.",
       image: "/images/image5b.webp",
     },
     {
-      riddle: "I'm orange and long, and rabbits love me. What am I?",
+      riddle: "I am orange and long, and rabbits love me. What am I?",
       answer: "Carrot",
       benefit: "High in beta-carotene, which is good for eye health and immune function.",
       image: "/images/image6b.webp",
     },
     {
-      riddle: "I'm a tree nut that looks like a brain. What am I?",
+      riddle: "I am a tree nut that looks like a brain. What am I?",
       answer: "Walnut",
       benefit: "Rich in omega-3 fatty acids, which support brain function and heart health.",
       image: "/images/image7b.webp",
